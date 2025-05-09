4da7468d4ca18b6606c0df92a7531e7c1354707f
ilyjasmine <lindovanstruik@gmail.com>
2025-05-09 10:40:45 +0200

    Should be fixed now TwT
    I love her

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const options = {
     left: "unset", // default: "unset"
     time: "0.3s", // default: "0.3s"
     mixColor: "#fff", // default: "#fff"
-    backgroundColor: "#fff",  // default: "#fff"
+    backgroundColor: "#0d1117",  // default: "#fff"
     buttonColorDark: "#100f2c",  // default: "#100f2c"
     buttonColorLight: "#fff", // default: "#fff"
     saveInCookies: true, // default: true,
